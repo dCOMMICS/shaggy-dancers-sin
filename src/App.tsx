bade4d4ae4fa92974b6e81edfa4c90daba7f106a
dCOMMICS <sanwurld@gmail.com>
2023-08-23 00:00:23 +0300

    code works fine for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,75 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
-import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+import { Refine } from "@refinedev/core";
+import {
+    notificationProvider,
+    ErrorComponent,
+    ThemedLayout,
+} from "@refinedev/antd";
+import routerProvider, { NavigateToResource } from "@refinedev/react-router-v6";
+import dataProvider from "@refinedev/simple-rest";
 
-import { notificationProvider } from "@refinedev/antd";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+
+import { AntdInferencer } from "@refinedev/inferencer/antd";
+
 import "@refinedev/antd/dist/reset.css";
 
-import routerBindings, {
-  DocumentTitleHandler,
-  UnsavedChangesNotifier,
-} from "@refinedev/react-router-v6";
-import dataProvider from "@refinedev/simple-rest";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ColorModeContextProvider } from "./contexts/color-mode";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <GitHubBanner />
-      <RefineKbarProvider>
-        <ColorModeContextProvider>
-          <Refine
-            notificationProvider={notificationProvider}
-            routerProvider={routerBindings}
-            dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
-            options={{
-              syncWithLocation: true,
-              warnWhenUnsavedChanges: true,
-              projectId: "cCOwAV-0PbotD-lqZjCJ",
-            }}
-          >
-            <Routes>
-              <Route index element={<WelcomePage />} />
-            </Routes>
-            <RefineKbar />
-            <UnsavedChangesNotifier />
-            <DocumentTitleHandler />
-          </Refine>
-        </ColorModeContextProvider>
-      </RefineKbarProvider>
-    </BrowserRouter>
-  );
-}
+const App: React.FC = () => {
+    return (
+        <BrowserRouter>
+            <Refine
+                routerProvider={routerProvider}
+                dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+                notificationProvider={notificationProvider}
+                resources={[
+                    {
+                        name: "blog_posts",
+                        list: "/blog-posts",
+                        show: "/blog-posts/show/:id",
+                        create: "/blog-posts/create",
+                        edit: "/blog-posts/edit/:id",
+                        meta: { canDelete: true },
+                    },
+                    {
+                        name: "categories",
+                        list: "/categories",
+                        show: "/categories/show/:id",
+                    },
+                ]}
+            >
+                <Routes>
+                    <Route
+                        element={
+                            <ThemedLayout>
+                                <Outlet />
+                            </ThemedLayout>
+                        }
+                    >
+                        <Route index element={<NavigateToResource />} />
+                        <Route path="blog-posts">
+                            <Route index element={<AntdInferencer />} />
+                            <Route
+                                path="show/:id"
+                                element={<AntdInferencer />}
+                            />
+                            <Route path="create" element={<AntdInferencer />} />
+                            <Route
+                                path="edit/:id"
+                                element={<AntdInferencer />}
+                            />
+                        </Route>
+                        <Route path="categories">
+                            <Route index element={<AntdInferencer />} />
+                            <Route
+                                path="show/:id"
+                                element={<AntdInferencer />}
+                            />
+                        </Route>
+                        <Route path="*" element={<ErrorComponent />} />
+                    </Route>
+                </Routes>
+            </Refine>
+        </BrowserRouter>
+    );
+};
 
 export default App;
